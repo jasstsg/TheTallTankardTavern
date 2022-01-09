d67b735eee50e0e82e1170b762834a16c6a2d14a
Tyler Jass <jasstsg@gmail.com>
2022-01-09 01:19:39 -0500

    site hosted only without IIS now, made the hide property on features edittable

--- TheTallTankardTavern/Program.cs
+++ TheTallTankardTavern/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Session;
+using System.Net;
 
 namespace TheTallTankardTavern
 {
@@ -22,6 +23,7 @@ namespace TheTallTankardTavern
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://TheTallTankardTavern");
                 });
     }
 }
